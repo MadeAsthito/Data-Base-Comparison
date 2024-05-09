b606d8922d73ff68e6400431855cc4e43dbc1d11
Asthito <madeasthito.yogi@gmail.com>
2024-05-09 15:22:10 +0800

    add testing relational to main

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import global_func as gf
+import db_relational as relational
 
 # MAIN
 if __name__ == '__main__':
-
-    # Input untuk measure_query_execution
-    query: str = "SELECT penjualan.* FROM penjualan INNER JOIN detail_penjualan ON penjualan.id_penjualan = detail_penjualan.id_penjualan WHERE penjualan.id_penjualan = 1"
-    iterations: int = 1000
-
-    gf.get_db_size()
-    print()
-
-    print("Query with no Caching")
-    gf.measure_query_execution(query, iterations)
-    print()
-
-    print("Query with Caching")
-    gf.measure_query_execution(query, iterations, True)
-    print()
-
-    gf.measure_select_generated_data()
-    print()
-
-    gf.measure_insert_generated_data(iterations)
+    # DATABASE RELATIONAL
+    relational.test_relational()
